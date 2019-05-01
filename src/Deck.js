b7409410d2e88f3c85120ae81e2399234e534834
Seil Cho <seilcho7@hotmail.com>
2019-05-01 07:57:36 -0400

    hide bunnton ternary adds

--- src/Deck.js
+++ src/Deck.js
@@ -43,8 +43,8 @@ class Deck extends React.Component {
                     </div>
                         {this.state.dealerTotal ? <h4>Dealer total: {this.state.dealerTotal}</h4> : null}
                     <br></br>
-                <button onClick={this._hitCard}>Hit</button>
-                <button onClick={this.state.deck ? this._showDealer : null}>Stop</button>
+                {this.state.p1 ? <button onClick={this._hitCard}>Hit</button> : null}
+                {this.state.p1 ? <button onClick={this.state.deck ? this._showDealer : null}>Stop</button> : null}
             </div>
         )
     }
